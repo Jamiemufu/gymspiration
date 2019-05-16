a6fd2944bb1e30ffe3f6a77860f2db8e0b430948
Jamie Evans <taichi.chikung@gmail.com>
2019-05-16 09:18:30 +0100

    quick login front end amends

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,51 +3,53 @@
 @section('content')
 
 <div class="flex-container">
-    <div class="form-container">
+    <div class="login">
 
-        <div class="logo">
-            <img src="{{ asset('images/sidebar_logo.png') }}" alt="Gymspiration Logo">
-            <h1>GYMSPIRATION</h1>
+        <div class="form-container">
+
+            <div class="logo">
+                <img src="{{ asset('images/sidebar_logo.png') }}" alt="Gymspiration Logo">
+                <h1>GYMSPIRATION</h1>
+            </div>
+
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+                @error('username')
+                <span class="" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+
+                @error('password')
+                <span class="" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+
+                <div class="username">
+                    <label for="username">Username:
+                        <input type="text" name="username" required autofocus>
+                    </label>
+                </div>
+
+                <div class="password">
+                    <label for="password"> Password:
+                        <input type="text" name="password" required autocomplete="current-password">
+                    </label>
+                </div>
+
+                <div class="checkbox">
+                    <input name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
+                    <label for="remember" class="remember">Remember me?</label>
+                </div>
+
+                <div class="button">
+                    <button type="submit">Login</button>
+                </div>
+
+            </form>
         </div>
-       
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            @error('username')
-            <span class="" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-            
-            @error('password')
-            <span class="" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-            @enderror
-            
-            <div class="username">
-                <label for="username">Username:
-                    <input type="text" name="username" required autofocus>
-                </label>
-            </div>
-
-            <div class="password">
-                <label for="password"> Password:
-                    <input type="text" name="password" required
-                        autocomplete="current-password">
-                </label>
-            </div>
-
-            <div class="checkbox">
-                <input name="remember" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
-                <label for="remember" class="remember">Remember me?</label>
-            </div>           
-
-            <div class="button">
-                <button type="submit">Login</button>
-            </div>
-
-        </form>
     </div>
 </div>
 
